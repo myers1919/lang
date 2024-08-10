f246c3ef328fd82df6ffa767772b3dd20c388509
Matt Myers <matt.my19@gmail.com>
2024-08-10 10:44:23 -0400

    Added controller with update and render methods.

--- gamestate.py
+++ gamestate.py
@@ -1,3 +1,10 @@
 class Gamestate:
     def __init__(self):
+        self.current_state = None
+    
+    def get_state(self):
+        if self.current_state == None:
+            self.current_state = 'vocab'
+
+    def update(self):
         pass
